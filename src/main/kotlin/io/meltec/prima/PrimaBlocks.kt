797170270b99431bb6b6a180f5acccbb60f895d2
Keyon Amirpanahi <keyon@keyoninfo.com>
2021-05-26 22:44:53 -0500

    Add mining levels to tier 1 ores

--- src/main/kotlin/io/meltec/prima/PrimaBlocks.kt
+++ src/main/kotlin/io/meltec/prima/PrimaBlocks.kt
@@ -3,6 +3,7 @@ package io.meltec.prima
 import io.meltec.prima.util.PrimaIdentifier
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags
 import net.minecraft.block.Block
 import net.minecraft.block.Material
 import net.minecraft.item.BlockItem
@@ -10,9 +11,12 @@ import net.minecraft.item.ItemGroup
 import net.minecraft.util.registry.Registry
 
 object PrimaBlocks {
-    val COPPER_ORE = Block(FabricBlockSettings.of(Material.STONE).requiresTool().strength(3.0f))
-    val TIN_ORE = Block(FabricBlockSettings.of(Material.STONE).requiresTool().strength(3.0f))
-    val ZINC_ORE = Block(FabricBlockSettings.of(Material.STONE).requiresTool().strength(3.0f))
+    val COPPER_ORE = Block(FabricBlockSettings.of(Material.STONE).requiresTool().strength(3.0f)
+        .breakByTool(FabricToolTags.PICKAXES, 1))
+    val TIN_ORE = Block(FabricBlockSettings.of(Material.STONE).requiresTool().strength(3.0f)
+        .breakByTool(FabricToolTags.PICKAXES, 1))
+    val ZINC_ORE = Block(FabricBlockSettings.of(Material.STONE).requiresTool().strength(3.0f)
+        .breakByTool(FabricToolTags.PICKAXES, 1))
 
     fun register() {
         Registry.register(Registry.BLOCK, PrimaIdentifier("copper_ore"), COPPER_ORE)
