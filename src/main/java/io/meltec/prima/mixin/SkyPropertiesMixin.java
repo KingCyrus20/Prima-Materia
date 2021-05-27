0f3877c3d2d585107c13dff46a399e60c4c77461
Keyon Amirpanahi <keyon@keyoninfo.com>
2021-05-27 18:23:51 -0500

    Increase cloud height to 192

---
+++ src/main/java/io/meltec/prima/mixin/SkyPropertiesMixin.java
@@ -0,0 +1,23 @@
+package io.meltec.prima.mixin;
+
+import net.minecraft.client.render.SkyProperties;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(SkyProperties.class)
+public abstract class SkyPropertiesMixin{
+    @Shadow
+    private float cloudsHeight;
+    @Shadow
+    private SkyProperties.SkyType skyType;
+
+    @Inject(method = "<init>(FZLnet/minecraft/client/render/SkyProperties$SkyType;ZZ)V", at = @At("TAIL"))
+    private void SkyProperties(float cloudsHeight, boolean alternateSkyColor, SkyProperties.SkyType skyType,
+                               boolean brightenLighting, boolean darkened, CallbackInfo ci){
+        if (this.skyType == SkyProperties.SkyType.NORMAL)
+            this.cloudsHeight = 192.0f;
+    }
+}
