01096ea69a5c352ca0ff7471ede7f196e0cac510
Keyon Amirpanahi <keyon@keyoninfo.com>
2021-05-27 19:30:07 -0500

    Use BiomeModifications for ore generation

--- src/main/kotlin/io/meltec/prima/PrimaOres.kt
+++ src/main/kotlin/io/meltec/prima/PrimaOres.kt
@@ -1,9 +1,12 @@
 package io.meltec.prima
 
 import io.meltec.prima.util.PrimaIdentifier
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors
 import net.minecraft.util.registry.BuiltinRegistries
 import net.minecraft.util.registry.Registry
 import net.minecraft.util.registry.RegistryKey
+import net.minecraft.world.gen.GenerationStep
 import net.minecraft.world.gen.decorator.Decorator
 import net.minecraft.world.gen.decorator.RangeDecoratorConfig
 import net.minecraft.world.gen.feature.Feature
@@ -52,17 +55,20 @@ object PrimaOres {
             PrimaIdentifier("ore_copper_overworld")
         )
         Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, oreCopperOverworld.value, ORE_COPPER_OVERWORLD)
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, oreCopperOverworld)
 
         val oreTinOverworld = RegistryKey.of(
             Registry.CONFIGURED_FEATURE_WORLDGEN,
             PrimaIdentifier("ore_tin_overworld")
         )
         Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, oreTinOverworld.value, ORE_TIN_OVERWORLD)
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, oreTinOverworld)
 
         val oreZincOverworld = RegistryKey.of(
             Registry.CONFIGURED_FEATURE_WORLDGEN,
             PrimaIdentifier("ore_zinc_overworld")
         )
         Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, oreZincOverworld.value, ORE_ZINC_OVERWORLD)
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, oreZincOverworld)
     }
 }
