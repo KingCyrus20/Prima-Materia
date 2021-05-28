156f2857a7bf32412379acd66a9e67836765c906
Keyon Amirpanahi <keyon@keyoninfo.com>
2021-05-28 17:51:24 -0500

    Add glider and wing items, change item group names

---
+++ src/main/kotlin/io/meltec/prima/item/GliderItem.kt
@@ -0,0 +1,10 @@
+package io.meltec.prima.item
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.minecraft.entity.EquipmentSlot
+import net.minecraft.item.ArmorItem
+import net.minecraft.item.ArmorMaterials
+
+object GliderItem:ArmorItem(ArmorMaterials.LEATHER, EquipmentSlot.CHEST, FabricItemSettings().group(PrimaItems.EQUIPPABLE_GROUP)) {
+
+}
--- src/main/kotlin/io/meltec/prima/item/PrimaItems.kt
+++ src/main/kotlin/io/meltec/prima/item/PrimaItems.kt
@@ -2,15 +2,28 @@ package io.meltec.prima.item
 
 import io.meltec.prima.util.PrimaIdentifier
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 import net.minecraft.util.registry.Registry
 
 object PrimaItems {
-    val PRIMARY_GROUP = FabricItemGroupBuilder.create(PrimaIdentifier("general"))
+    val GENERAL_GROUP = FabricItemGroupBuilder.create(PrimaIdentifier("general"))
+        .build()
+    val TOOL_GROUP = FabricItemGroupBuilder.create(PrimaIdentifier("tool"))
         .icon { ItemStack(BowDrillItem) }
         .build()
+    val EQUIPPABLE_GROUP = FabricItemGroupBuilder.create(PrimaIdentifier("equippable"))
+        .icon { ItemStack(GliderItem) }
+        .build()
+
+    val GliderLeftWingItem = Item(FabricItemSettings().group(GENERAL_GROUP))
+    val GliderRightWingItem = Item(FabricItemSettings().group(GENERAL_GROUP))
 
     fun register() {
         Registry.register(Registry.ITEM, PrimaIdentifier("bow_drill"), BowDrillItem)
+        Registry.register(Registry.ITEM, PrimaIdentifier("glider_left_wing"), GliderLeftWingItem)
+        Registry.register(Registry.ITEM, PrimaIdentifier("glider_right_wing"), GliderRightWingItem)
+        Registry.register(Registry.ITEM, PrimaIdentifier("glider"), GliderItem)
     }
 }
