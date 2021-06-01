4cb03cf3ddc5bde45712f4f6c32bbac5e2c690eb
Keyon Amirpanahi <keyon@keyoninfo.com>
2021-06-01 01:39:08 -0500

    Add flint tool material and sword item

---
+++ src/main/kotlin/io/meltec/prima/FlintToolMaterial.kt
@@ -0,0 +1,31 @@
+package io.meltec.prima
+
+import net.minecraft.item.Items
+import net.minecraft.item.ToolMaterial
+import net.minecraft.recipe.Ingredient
+
+class FlintToolMaterial : ToolMaterial {
+  override fun getDurability(): Int {
+    return 100
+  }
+
+  override fun getMiningSpeedMultiplier(): Float {
+    return 4.0f
+  }
+
+  override fun getAttackDamage(): Float {
+    return 0f
+  }
+
+  override fun getMiningLevel(): Int {
+    return 1
+  }
+
+  override fun getEnchantability(): Int {
+    return 0
+  }
+
+  override fun getRepairIngredient(): Ingredient {
+    return Ingredient.ofItems(Items.FLINT)
+  }
+}
--- src/main/kotlin/io/meltec/prima/item/PrimaItems.kt
+++ src/main/kotlin/io/meltec/prima/item/PrimaItems.kt
@@ -1,25 +1,34 @@
 package io.meltec.prima.item
 
+import io.meltec.prima.FlintToolMaterial
 import io.meltec.prima.util.PrimaIdentifier
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
 import net.minecraft.item.Item
+import net.minecraft.item.ItemGroup
 import net.minecraft.item.ItemStack
+import net.minecraft.item.SwordItem
 import net.minecraft.util.registry.Registry
 
 object PrimaItems {
-  val GENERAL_GROUP = FabricItemGroupBuilder.create(PrimaIdentifier("general")).build()
-  val TOOL_GROUP =
+  val GENERAL_GROUP: ItemGroup = FabricItemGroupBuilder.create(PrimaIdentifier("general")).build()
+  val TOOL_GROUP: ItemGroup =
       FabricItemGroupBuilder.create(PrimaIdentifier("tool"))
           .icon { ItemStack(BowDrillItem) }
           .build()
-  val EQUIPPABLE_GROUP =
+  val COMBAT_GROUP: ItemGroup =
+      FabricItemGroupBuilder.create(PrimaIdentifier("combat"))
+          .icon { ItemStack(FLINT_SWORD) }
+          .build()
+  val EQUIPPABLE_GROUP: ItemGroup =
       FabricItemGroupBuilder.create(PrimaIdentifier("equippable"))
           .icon { ItemStack(GliderItem) }
           .build()
 
   val GliderLeftWingItem = Item(FabricItemSettings().group(GENERAL_GROUP))
   val GliderRightWingItem = Item(FabricItemSettings().group(GENERAL_GROUP))
+  val FLINT_SWORD =
+      SwordItem(FlintToolMaterial(), 5, 1.6f, FabricItemSettings().group(COMBAT_GROUP))
 
   fun register() {
     Registry.register(Registry.ITEM, PrimaIdentifier("bow_drill"), BowDrillItem)
