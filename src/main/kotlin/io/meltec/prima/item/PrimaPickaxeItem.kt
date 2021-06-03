f18b69ed62e85676f18f1094e90895a17dd07f61
Keyon Amirpanahi <keyon@keyoninfo.com>
2021-06-02 22:23:24 -0500

    Remove Cardinal Components API

--- src/main/kotlin/io/meltec/prima/item/PrimaPickaxeItem.kt
+++ src/main/kotlin/io/meltec/prima/item/PrimaPickaxeItem.kt
@@ -1,12 +1,7 @@
 package io.meltec.prima.item
 
-import io.meltec.prima.component.PrimaComponents
-import net.minecraft.client.item.TooltipContext
-import net.minecraft.item.ItemStack
 import net.minecraft.item.PickaxeItem
 import net.minecraft.item.ToolMaterial
-import net.minecraft.text.Text
-import net.minecraft.world.World
 
 class PrimaPickaxeItem(
     material: ToolMaterial,
@@ -14,13 +9,5 @@ class PrimaPickaxeItem(
     attackSpeed: Float,
     settings: Settings
 ) : PickaxeItem(material, attackDamage, attackSpeed, settings) {
-  override fun appendTooltip(
-      stack: ItemStack,
-      world: World?,
-      tooltip: MutableList<Text>,
-      context: TooltipContext?
-  ) {
-    super.appendTooltip(stack, world, tooltip, context)
-    tooltip.add(Text.of("Quality: " + PrimaComponents().QUALITY.get(stack).quality))
-  }
+
 }
