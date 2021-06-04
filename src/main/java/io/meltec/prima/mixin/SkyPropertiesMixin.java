df57bddd8fcf0fccd55e8a39ea406145234a27d7
Miguel Obregon <obregon@google.com>
2021-06-04 21:33:47 +0900

    Change SkyPropertiesMixin to use Accessors rather than Shadows

--- src/main/java/io/meltec/prima/mixin/SkyPropertiesMixin.java
+++ src/main/java/io/meltec/prima/mixin/SkyPropertiesMixin.java
@@ -3,15 +3,21 @@ package io.meltec.prima.mixin;
 import net.minecraft.client.render.SkyProperties;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(SkyProperties.class)
 public abstract class SkyPropertiesMixin {
-  @Shadow private float cloudsHeight;
-  @Final @Shadow private SkyProperties.SkyType skyType;
+  @Accessor("skyType")
+  abstract SkyProperties.SkyType getSkyType();
+
+  @SuppressWarnings("SameParameterValue")
+  @Accessor("cloudsHeight")
+  abstract void setCloudsHeight(float cloudsHeight);
 
   @Inject(
       method = "<init>(FZLnet/minecraft/client/render/SkyProperties$SkyType;ZZ)V",
@@ -23,6 +29,6 @@ public abstract class SkyPropertiesMixin {
       boolean brightenLighting,
       boolean darkened,
       CallbackInfo ci) {
-    if (this.skyType == SkyProperties.SkyType.NORMAL) this.cloudsHeight = 192.0f;
+    if (getSkyType() == SkyProperties.SkyType.NORMAL) setCloudsHeight(192f);
   }
 }
