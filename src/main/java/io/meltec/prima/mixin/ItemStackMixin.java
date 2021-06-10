62716ce806732a6e5a5ddb65275bb262745eafed
Keyon Amirpanahi <keyon@keyoninfo.com>
2021-06-10 01:27:50 -0500

    Make ItemStackMixin for custom tool durability

---
+++ src/main/java/io/meltec/prima/mixin/ItemStackMixin.java
@@ -0,0 +1,28 @@
+package io.meltec.prima.mixin;
+
+import io.meltec.prima.item.PrimaPickaxe;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundTag;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ItemStack.class)
+public class ItemStackMixin {
+    @Final
+    @Shadow
+    private Item item;
+    @Shadow
+    private CompoundTag tag;
+
+    @Inject(method = "getMaxDamage", at = @At("HEAD"), cancellable = true)
+    public void getMaxDamage(CallbackInfoReturnable<Integer> ci) {
+        if (item instanceof PrimaPickaxe && tag.contains("durability")) {
+            ci.setReturnValue(tag.getInt("durability"));
+        }
+    }
+}
